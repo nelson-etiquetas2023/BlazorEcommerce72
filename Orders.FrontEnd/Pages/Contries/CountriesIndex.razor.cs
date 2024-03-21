2c8c67c3980b21c2f0f65bc905c9c98a8d64dd87
nelson pino <User@WINDOWS-P4VOGQ5>
2024-03-21 07:12:01 -0400

    separacion del code behind y html de los componentes de blazor

---
+++ Orders.FrontEnd/Pages/Contries/CountriesIndex.razor.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Components;
+using Orders.FrontEnd.Repositories;
+using Orders.Shared.Entities;
+
+namespace Orders.FrontEnd.Pages.Contries
+{
+    public partial class CountriesIndex
+    {
+        [Inject] private IRepository Repository { get; set; } = null!;
+        public List<Country>? Countries { get; set; }
+
+        protected async override Task OnInitializedAsync()
+        {
+            var responseHttp = await Repository.GetAsync<List<Country>>("api/Countries");
+            Countries = responseHttp.Response!;
+
+        }
+    }
+}
+ 
